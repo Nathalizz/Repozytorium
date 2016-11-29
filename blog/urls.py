bb4df3cdda8d1009bd83d50ccb4b2ffdd0643829
Natalia <zon_natalia@poczta.fm>
2016-11-29 22:03:06 +0100

    13 commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/data$', views.post_data, name='post_data'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,7 @@ def  post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def  post_data(request):
+    posts=Post.objects.all()
+    return render(request, 'blog/post_data.html', {'klucz': posts})
