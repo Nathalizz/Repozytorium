378db227463f56091b1218a25047b3a6906dc428
Natalia <zon_natalia@poczta.fm>
2016-12-08 23:39:31 +0100

    16 commit

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from .models import Post
 
 from django.shortcuts import render, get_object_or_404
 
-def  post_list(request):
+def post_list(request):
     posts=Post.objects.all()
     return render(request, 'blog/post_list.html', {'klucz': posts})
 
@@ -12,6 +12,8 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-def  post_data(request):
-    posts=Post.objects.all()
-    return render(request, 'blog/post_data.html', {'klucz': posts})
+def post_data(request):
+    return render(request, 'blog/post_data.html')
+
+def zyczenia(request):
+    return render(request, 'blog/zyczenia.html')
